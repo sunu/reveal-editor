e0ddf35e096790de22d4060fdcbedd52352945b2
Tarashish Mishra <sunu0000@gmail.com>
2014-03-09 01:16:27 +0530

    Initial commit.

---
+++ js/editor.js
@@ -0,0 +1,43 @@
+(function (global, document) {
+
+    function init () {
+        var addButton = document.getElementById("add");
+        addButton.addEventListener("click", onAddClick);
+        initTinyMCE();
+    }
+
+    function initTinyMCE () {
+        tinymce.init({
+            selector: "section",
+            theme: "modern",
+            add_unload_trigger: false,
+            schema: "html5",
+            inline: true,
+            plugins: [
+                "advlist autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table contextmenu paste"
+            ],
+            toolbar1: "styleselect | bold italic | alignleft aligncenter alignright alignjustify",
+            toolbar2: "bullist numlist outdent indent | link image media",
+            statusbar: true,
+            fixed_toolbar_container: "#toolbar",
+            menubar: false,
+        });
+    }
+
+    function onAddClick () {
+        var template = "<h1>A NEW SLIDE</h1>\
+                    <h3>An awesome one.</h3>\
+                    <p>Sample text.</p>"
+        var element = document.createElement('section');
+        element.innerHTML = template;
+        var slides = document.getElementsByClassName('slides')[0];
+        slides.appendChild(element);
+        initTinyMCE();
+        Reveal.next();
+        tinyMCE.activeEditor.focus();
+    }
+
+    init();
+})(window, window.document);
