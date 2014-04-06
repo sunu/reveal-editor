7cf34bc03d86edc6e570a8721ec33969018a5228
Tarashish Mishra <sunu0000@gmail.com>
2014-04-07 00:09:22 +0530

    Replace tinymce with etch.

--- static/js/editor.js
+++ static/js/editor.js
@@ -5,27 +5,6 @@
         addButton.addEventListener("click", onAddClick);
         var saveButton = document.getElementById("save");
         saveButton.addEventListener("click", onSaveClick);
-        initTinyMCE();
-    }
-
-    function initTinyMCE () {
-        tinymce.init({
-            selector: "section",
-            theme: "modern",
-            add_unload_trigger: false,
-            schema: "html5",
-            inline: true,
-            plugins: [
-                "advlist autolink lists link image charmap print preview anchor",
-                "searchreplace visualblocks code fullscreen",
-                "insertdatetime media table contextmenu paste"
-            ],
-            toolbar1: "styleselect | bold italic | alignleft aligncenter alignright alignjustify",
-            toolbar2: "bullist numlist outdent indent | link image media",
-            statusbar: true,
-            fixed_toolbar_container: "#toolbar",
-            menubar: false,
-        });
     }
 
     function onAddClick () {
@@ -36,9 +15,7 @@
         element.innerHTML = template;
         var slides = document.getElementsByClassName('slides')[0];
         slides.appendChild(element);
-        initTinyMCE();
         Reveal.next();
-        tinyMCE.activeEditor.focus();
     }
 
     function onSaveClick () {
